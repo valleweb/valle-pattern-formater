a6ffaa3459bdc874fdf9e5a9af8170df401de5db
SabrinaBarros <sabrina.barros2045@gmail.com>
2021-06-17 02:47:42 -0300

    fix(unit-tests): x pattern #8

--- draftToInternationalXPatterns.test.js
+++ draftToInternationalXPatterns.test.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 
 test('Should format a CPF pattern', () => {
 
-    const result = patternFormater('XXX.XXX.XXX-XX', '09843476490');
+    const result = patternFormater.draftToInternational('XXX.XXX.XXX-XX', '09843476490');
 
     chai.expect(result).to.equal('098.434.764-90');
 
@@ -11,7 +11,7 @@ test('Should format a CPF pattern', () => {
 
 test('Should format a CNPJ pattern', () => {
 
-    const result = patternFormater('XX.XXX.XXX/XXXX-XX', '29701763000141');
+    const result = patternFormater.draftToInternational('XX.XXX.XXX/XXXX-XX', '29701763000141');
 
     chai.expect(result).to.equal('29.701.763/0001-41');
 
@@ -19,7 +19,7 @@ test('Should format a CNPJ pattern', () => {
 
 test('Should format a CEP pattern', () => {
 
-    const result = patternFormater('XXXXX-XXX', '74986290');
+    const result = patternFormater.draftToInternational('XXXXX-XXX', '74986290');
 
     chai.expect(result).to.equal('74986-290');
 
@@ -27,7 +27,7 @@ test('Should format a CEP pattern', () => {
 
 test('Should format a short date pattern', () => {
 
-    const result = patternFormater('XX/XX/XX', '031192');
+    const result = patternFormater.draftToInternational('XX/XX/XX', '031192');
 
     chai.expect(result).to.equal('03/11/92');
 
@@ -35,7 +35,7 @@ test('Should format a short date pattern', () => {
 
 test('Should format a complete date pattern', () => {
 
-    const result = patternFormater('XX/XX/XXXX', '03111992');
+    const result = patternFormater.draftToInternational('XX/XX/XXXX', '03111992');
 
     chai.expect(result).to.equal('03/11/1992');
 
@@ -43,7 +43,7 @@ test('Should format a complete date pattern', () => {
 
 test('Should format a financial count pattern', () => {
 
-    const result = patternFormater('XX.XXX', '28167');
+    const result = patternFormater.draftToInternational('XX.XXX', '28167');
 
     chai.expect(result).to.equal('28.167');
 
@@ -51,7 +51,7 @@ test('Should format a financial count pattern', () => {
 
 test('Should format a financial cost center pattern', () => {
 
-    const result = patternFormater('XX.XXXXX', '2816733');
+    const result = patternFormater.draftToInternational('XX.XXXXX', '2816733');
 
     chai.expect(result).to.equal('28.16733');
 
@@ -59,8 +59,8 @@ test('Should format a financial cost center pattern', () => {
 
 test('Should format a BR phone number pattern', () => {
 
-    const resultWith9 = patternFormater('+XX XX OXXXX-XXXX', '5511912345678');
-    const resultWithout9 = patternFormater('+XX XX OXXXX-XXXX', '551112345678');
+    const resultWith9 = patternFormater.draftToInternational('+XX XX OXXXX-XXXX', '5511912345678');
+    const resultWithout9 = patternFormater.draftToInternational('+XX XX OXXXX-XXXX', '551112345678');
 
     chai.expect(resultWith9).to.equal('+55 11 91234-5678');
     chai.expect(resultWithout9).to.equal('+55 11 1234-5678');
@@ -69,7 +69,7 @@ test('Should format a BR phone number pattern', () => {
 
 test('Should format a RG pattern', () => {
 
-    const result = patternFormater('XX.XXX.XXX-X', '472383358');
+    const result = patternFormater.draftToInternational('XX.XXX.XXX-X', '472383358');
 
     chai.expect(result).to.equal('47.238.335-8');
 
