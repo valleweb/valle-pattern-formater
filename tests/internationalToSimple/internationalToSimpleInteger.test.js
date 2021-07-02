9f26977560a97c12ee9d872ac59f8c33ff11eded
SabrinaBarros <sabrina.barros2045@gmail.com>
2021-07-02 00:32:02 -0300

    feat(unit-tests): refactor internationalToSimple folder structure

---
+++ tests/internationalToSimple/internationalToSimpleInteger.test.js
@@ -0,0 +1,78 @@
+const patternFormater = require('../../src/patternFormater');
+const chai = require('chai');
+
+describe('Should format integer numbers with ##,#00 pattern', () => {
+
+    test('Should format 100', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '100');
+
+        chai.expect(result).to.equal('100');
+
+    });
+
+    test('Should format 1.000', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '1.000');
+
+        chai.expect(result).to.equal('1000');
+
+    });
+
+    test('Should format 10.000', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '10.000');
+
+        chai.expect(result).to.equal('10000');
+
+    });
+
+    test('Should format 100.000', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '100.000');
+
+        chai.expect(result).to.equal('100000');
+
+    });
+
+    test('Should format 1.000.000', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '1.000.000');
+
+        chai.expect(result).to.equal('1000000');
+
+    });
+
+    test('Should format 1.000.000.000', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '1.000.000.000');
+
+        chai.expect(result).to.equal('1000000000');
+
+    });
+
+    test('Should format 1.000', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '1.000');
+
+        chai.expect(result).to.equal('1000');
+
+    });
+
+    test('Should format 0', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '0');
+
+        chai.expect(result).to.equal('0');
+
+    });
+
+    test('Should format 89.756.465', () => {
+
+        const result = patternFormater.internationalToSimple('##,#00', '89.756.465');
+
+        chai.expect(result).to.equal('89756465');
+
+    });
+
+});
